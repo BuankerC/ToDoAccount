9296c000ca6b6fdc2d84e0bb616c3cdf2f141605
Seon Yang <ergaster91@gmail.com>
2019-10-17 10:02:34 +0900

    signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login
 
 # Create your views here.
+
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            auth_login(request, user)
+            return redirect('todos:index')
+    else:
+        form = UserCreationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/signup.html', context)
